b68ae66ca254fdaa7cf8c989cd001b1264937bd5
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-14 18:57:58 -0300

    Lil context menu for creating nodes

---
+++ CharterUI.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var empty_menu := $EmptyRMBClickContextMenu
+
+func _process(_delta: float) -> void:
+	# Right click
+	if Input.is_action_just_pressed("right_click"):
+		if false: # Replace with a code to check if mouse is on node or link later
+			pass
+		else:
+			# Dsplay "empty click" menu
+			empty_menu.position = get_global_mouse_position()
+			empty_menu.visible = true
