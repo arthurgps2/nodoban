e3b0e36686174d48bd3ef9db8eff1bc899c719ce
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-14 19:25:43 -0300

    Creating nodes

--- CharterUI.gd
+++ CharterUI.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var node_scene := preload("res://CharterNode.tscn")
+
 @onready var empty_menu := $EmptyRMBClickContextMenu
 
 func _process(_delta: float) -> void:
@@ -11,3 +13,11 @@ func _process(_delta: float) -> void:
 			# Dsplay "empty click" menu
 			empty_menu.position = get_global_mouse_position()
 			empty_menu.visible = true
+
+
+func _on_empty_menu_index_pressed(index: int) -> void:
+	match index:
+		0:
+			var node := node_scene.instantiate()
+			node.position = get_local_mouse_position()
+			get_node("..").add_child(node)
