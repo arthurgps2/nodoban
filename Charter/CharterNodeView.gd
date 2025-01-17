c1a97e8d8b75202d17f4e6e8f065af79dae3de98
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-17 18:57:24 -0300

    Bug fix with context menus

--- Charter/CharterNodeView.gd
+++ Charter/CharterNodeView.gd
@@ -44,10 +44,14 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("right_click"):
 		var node_under = get_node_under_mouse()
 		if node_under:
+			empty_cmenu.visible = false
+			
 			node_cmenu.position = get_global_mouse_position()
 			node_cmenu.visible = true
 			node_in_context = node_under
 		else: # Clicked on the void
+			node_cmenu.visible = false
+			
 			empty_cmenu.position = get_global_mouse_position()
 			empty_cmenu.visible = true
 	
