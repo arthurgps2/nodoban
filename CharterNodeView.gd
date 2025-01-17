5923c0163bd6f9f5aabdbe0d8934bb4302c06708
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-17 15:21:31 -0300

    Zooming and dragging node view

--- CharterNodeView.gd
+++ CharterNodeView.gd
@@ -1,20 +1,43 @@
 extends Control
 
+const zoom_factor := .05
+
 var node_dragging : CharterNode = null
 var node_drag_offset : Vector2
+var view_dragging := false
+var view_drag_offset : Vector2
 
 func _process(delta: float) -> void:
+	# Node management
 	var mouse_pos := get_global_mouse_position()
 	
 	if Input.is_action_just_pressed("left_click"):
 		for child in get_children():
-			if child is CharterNode and child.get_rect().has_point(mouse_pos):
+			if child is CharterNode and child.get_global_rect().has_point(mouse_pos):
 				node_dragging = child
-				node_drag_offset = mouse_pos - node_dragging.position
+				node_drag_offset = mouse_pos - node_dragging.global_position
 				break
+		
+		if not node_dragging:
+			view_dragging = true
+			view_drag_offset = mouse_pos - position
 	
 	if Input.is_action_just_released("left_click"):
 		node_dragging = null
+		view_dragging = false
 	
+	# Dragging nodes
 	if node_dragging:
-		node_dragging.position = mouse_pos - node_drag_offset
+		node_dragging.global_position = mouse_pos - node_drag_offset
+	# Dragging view
+	elif view_dragging:
+		position = mouse_pos - view_drag_offset
+	
+	# Zoom in/out
+	if Input.is_action_just_pressed("scroll_up"):
+		var add := zoom_factor * scale
+		scale += add
+	
+	if Input.is_action_just_pressed("scroll_down"):
+		var sub = zoom_factor * scale
+		scale -= sub
