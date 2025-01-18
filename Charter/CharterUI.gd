9bae88076fd6ad6dd2bac1ad6daa3f64ada75a66
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-18 19:05:32 -0300

    Options menu options working

--- Charter/CharterUI.gd
+++ Charter/CharterUI.gd
@@ -1,3 +1,15 @@
 extends Control
 
-# This dude'll have another purpose, don't worry
+func _ready() -> void:
+	%OptionsMenuButton.get_popup().index_pressed.connect(_on_options_menu_index_pressed)
+
+func _on_options_menu_index_pressed(index : int):
+	match index:
+		0: # Save file
+			$FileDialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+			$FileDialog.visible = true
+		1: # Load file
+			$FileDialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+			$FileDialog.visible = true
+		2: # Node types
+			print("open node types")
