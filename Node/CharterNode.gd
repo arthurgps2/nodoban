5f2faa5fb1a0bbb165986ecacafef5a0622013c1
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-18 19:24:39 -0300

    Deleting links connected to nodes being deleted

--- Node/CharterNode.gd
+++ Node/CharterNode.gd
@@ -5,4 +5,7 @@ func get_center() -> Vector2:
 	return position + size/2
 
 func delete() -> void:
+	for child in get_node("..").get_children():
+		if child is CharterLink and (child.start_node == self or child.end_node == self):
+			child.queue_free()
 	queue_free()
