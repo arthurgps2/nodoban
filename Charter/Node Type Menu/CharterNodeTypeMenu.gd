3915b5e6fdc918e8049b86e2ad3514a66bb745c7
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-19 15:14:55 -0300

    Node type menu dropdown button working

--- Charter/Node Type Menu/CharterNodeTypeMenu.gd
+++ Charter/Node Type Menu/CharterNodeTypeMenu.gd
@@ -1,11 +1,5 @@
+class_name CharterNodeTypeMenu
 extends VBoxContainer
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_dropdown_button_pressed() -> void:
+	$MenuBody.visible = not $MenuBody.visible
