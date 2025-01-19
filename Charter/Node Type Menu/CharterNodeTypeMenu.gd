f04b9d0f4126f6c96512b19b91278b8e9deb2897
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-19 15:26:55 -0300

    Some visual changes to node types

--- Charter/Node Type Menu/CharterNodeTypeMenu.gd
+++ Charter/Node Type Menu/CharterNodeTypeMenu.gd
@@ -1,5 +1,5 @@
 class_name CharterNodeTypeMenu
-extends VBoxContainer
+extends PanelContainer
 
 func _on_dropdown_button_pressed() -> void:
-	$MenuBody.visible = not $MenuBody.visible
+	%MenuBody.visible = not %MenuBody.visible
