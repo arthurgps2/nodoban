d3e17df47834b15f215af0f4139966acc9777a29
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-20 12:41:07 -0300

    Method for retrieving value set in property

--- Charter/Node Type Menu/Node Property Value/CharterNodePropertyValue.gd
+++ Charter/Node Type Menu/Node Property Value/CharterNodePropertyValue.gd
@@ -8,6 +8,25 @@ enum ITEM_ID {
 	ARRAY = 3,
 }
 
+func get_value() -> Variant:
+	match %ValueType.selected:
+		ITEM_ID.STRING:
+			return %StringEdit.text
+		ITEM_ID.NUMBER:
+			var num_str : String = %NumberEdit.text
+			if num_str.is_valid_int(): return num_str.to_int()
+			elif num_str.is_valid_float(): return num_str.to_float()
+			else: return null
+		ITEM_ID.BOOL:
+			return %BoolEdit.button_pressed
+		ITEM_ID.ARRAY:
+			var return_value := []
+			for child in %ArrayEdit.get_children():
+				if child is not CharterNodePropertyValue: continue
+				return_value.append(child.get_value())
+			return return_value
+			
+		_: return null
 
 func _on_value_type_item_selected(index: int) -> void:
 	%StringEdit.visible = false
