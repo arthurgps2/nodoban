00ede99ab282beb101b9e5eeea4b9c7162fb7a5e
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2025-01-20 13:09:18 -0300

    Create new property button

--- Charter/Node Type Menu/CharterNodeTypeMenu.gd
+++ Charter/Node Type Menu/CharterNodeTypeMenu.gd
@@ -1,5 +1,13 @@
 class_name CharterNodeTypeMenu
 extends PanelContainer
 
+const node_property_scene : PackedScene = preload("res://Charter/Node Type Menu/Node Property/CharterNodeProperty.tscn")
+
 func _on_dropdown_button_pressed() -> void:
 	%MenuBody.visible = not %MenuBody.visible
+
+func _on_new_button_pressed() -> void:
+	var node_property := node_property_scene.instantiate()
+	%MenuBody/Items.add_child(node_property)
+	%MenuBody/Items.move_child(node_property, -1)
+	%MenuBody/Items.move_child(%MenuBody/Items/NewButton, -1)
